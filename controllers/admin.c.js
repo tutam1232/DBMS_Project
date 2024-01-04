65f850510181fdfc4b8f7a6ee3c051c1ec191ac1
kyuujin1602 <kurimataiza10@gmail.com>
2024-01-04 16:03:17 +0700

    Commit to branch "Nam"

--- controllers/admin.c.js
+++ controllers/admin.c.js
@@ -6,39 +6,54 @@ module.exports = {
             res.redirect('/');
         res.render("admin/option", {
             title: "Admin Option",
-            logout:true
+            logout: true
         });
     },
     renderViewMedicine: async function renderViewMedicine(req, res, next) {
-      if(!req.session.user)
-          res.redirect('/');
-      let ThongTinThuoc = null;
-      if(req.session.resolve == false)
-          ThongTinThuoc = await adminModel.selectMedicine(req, res, next);
-      else
-          ThongTinThuoc = await adminModel.selectMedicine_resolve(req, res, next);
-      //const LichCaNhan = await adminModel.selectPersonalSchedule(req, res, next);
+        if (!req.session.user)
+            res.redirect('/');
+        let ThongTinThuoc = null;
+        if (req.session.resolve == false)
+            ThongTinThuoc = await adminModel.selectMedicine(req, res, next);
+        else
+            ThongTinThuoc = await adminModel.selectMedicine_resolve(req, res, next);
+        //const LichCaNhan = await adminModel.selectPersonalSchedule(req, res, next);
 
-      res.render("admin/viewMedicine", {
-          title: "View Medicine",
-          ThongTinThuoc: ThongTinThuoc,
-          logout:true
-      });
+        res.render("admin/viewMedicine", {
+            title: "View Medicine",
+            ThongTinThuoc: ThongTinThuoc,
+            logout: true
+        });
     },
     rederAddMedicine: async function renderAddMedicine(req, res, next) {
-      if(!req.session.user)
-          res.redirect('/');
+        if (!req.session.user)
+            res.redirect('/');
 
-      //const LichCaNhan = await doctorModel.selectPersonalSchedule(req, res, next);
-      if(req.session.resolve == false)
-          adminModel.insertMedicine(req, res, next);
-      else
-          adminModel.insertMedicine_resolve(req, res, next);
+        //const LichCaNhan = await doctorModel.selectPersonalSchedule(req, res, next);
+        if (req.session.resolve == false)
+            adminModel.insertMedicine(req, res, next);
+        else
+            adminModel.insertMedicine_resolve(req, res, next);
 
-      res.render("admin/addMedicine", {
-          title: "Add Medicine",
-          //LichCaNhan: LichCaNhan,
-          logout:true
-      });
+        res.render("admin/addMedicine", {
+            title: "Add Medicine",
+            //LichCaNhan: LichCaNhan,
+            logout: true
+        });
     },
+
+    renderUpdateMedicine: async function renderUpdateMedicine(req, res, next) {
+        if (!req.session.user)
+            res.redirect('/');
+
+        if (req.session.resolve == false)
+            adminModel.updateMedicine(req, res, next);
+        else
+            adminModel.updateMedicine_resolve(req, res, next);
+
+        res.render("admin/updateMedicine", {
+            title: "Update Medicine",
+            logout: true
+        })
+    }
 }
--- routers/admin.r.js
+++ routers/admin.r.js
@@ -6,5 +6,7 @@ router.get("/", adminController.renderOption);
 router.get("/viewMedicine", adminController.renderViewMedicine);
 router.get("/addMedicine", adminController.rederAddMedicine);
 router.post("/addMedicine", adminController.rederAddMedicine);
+router.get("/updateMedicine", adminController.renderUpdateMedicine);
+router.post("/updateMedicine", adminController.renderUpdateMedicine);
 
 module.exports = router;
